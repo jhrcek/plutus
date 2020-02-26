c54d420c326d595899b2da3ad79bf053a5d5106d
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2020-02-26 12:47:20 +0000

    ci.nix for darwin too

--- ci.nix
+++ ci.nix
@@ -1,8 +1,14 @@
 let
-  packageSet = import ./default.nix {};
-  pkgs = packageSet.pkgs;
-in {
-  libs = packageSet.localLib.collectComponents' "library" packageSet.local-packages-new;
-  tests = packageSet.localLib.collectComponents' "tests" packageSet.local-packages-new;
-  benchmarks = packageSet.localLib.collectComponents' "benchmarks" packageSet.local-packages-new;
-}
+  systemBuilds = system:
+    let
+      packageSet = import ./default.nix { inherit system; };
+      pkgs = packageSet.pkgs;
+    in pkgs.recurseIntoAttrs {
+      libs = packageSet.localLib.collectComponents' "library" packageSet.local-packages-new;
+      tests = packageSet.localLib.collectComponents' "tests" packageSet.local-packages-new;
+      benchmarks = packageSet.localLib.collectComponents' "benchmarks" packageSet.local-packages-new;
+    };
+  linux = ["x86_64-linux"];
+  darwin = ["x86_64-darwin"];
+  systems = linux ++ darwin;
+in builtins.listToAttrs (builtins.map (system: { name = system; value = systemBuilds system; }) systems)
